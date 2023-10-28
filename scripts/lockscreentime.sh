22a62c5e1cbf95198e8f7041f8b81cbfa0c289b3
Eklavya Sood <eklavyasood@gmail.com>
2023-10-28 19:32:11 +0530

    main changes

--- scripts/lockscreentime.sh
+++ scripts/lockscreentime.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
-if [ -f "/usr/bin/swayidle" ]; then
-    echo "swayidle is installed."
-    swayidle -w timeout 1800 'swaylock -f' timeout 660 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
-else
-    echo "swayidle not installed."
-fi;
+#if [ -f "/usr/bin/swayidle" ]; then
+#    echo "swayidle is installed."
+#    swayidle -w timeout 1800 'swaylock -f' timeout 660 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+#else
+#    echo "swayidle not installed."
+#fi;
