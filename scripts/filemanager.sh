93eb18f55e38810d5590b5c7093c823a87029017
Eklavya Sood <eklavyasood@gmail.com>
2023-11-23 19:44:24 +0530

    we do a little bit of trolling

--- scripts/filemanager.sh
+++ scripts/filemanager.sh
@@ -1,18 +1,16 @@
 #/bin/sh
-#  _____ _ _                                                   
-# |  ___(_) | ___ _ __ ___   __ _ _ __   __ _  __ _  ___ _ __  
-# | |_  | | |/ _ \ '_ ` _ \ / _` | '_ \ / _` |/ _` |/ _ \ '__| 
-# |  _| | | |  __/ | | | | | (_| | | | | (_| | (_| |  __/ |    
-# |_|   |_|_|\___|_| |_| |_|\__,_|_| |_|\__,_|\__, |\___|_|    
-#                                             |___/            
-#  
-# by Stephan Raabe (2023) 
-# ----------------------------------------------------- 
+#  _____ _ _
+# |  ___(_) | ___ _ __ ___   __ _ _ __   __ _  __ _  ___ _ __
+# | |_  | | |/ _ \ '_ ` _ \ / _` | '_ \ / _` |/ _` |/ _ \ '__|
+# |  _| | | |  __/ | | | | | (_| | | | | (_| | (_| |  __/ |
+# |_|   |_|_|\___|_| |_| |_|\__,_|_| |_|\__,_|\__, |\___|_|
+#                                             |___/
+# -----------------------------------------------------
 
 if [ -f "/usr/bin/nautilus" ]; then
-    echo "nautilus is installed."
-    nautilus --new-window
+	echo "nautilus is installed."
+	nautilus --new-window
 else
-    echo "nautilus not installed. Launching thunar instead."
-    thunar
-fi;
+	echo "nautilus not installed. Launching thunar instead."
+	thunar
+fi
--- scripts/templates.sh
+++ scripts/templates.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-#  _____                    _       _             
-# |_   _|__ _ __ ___  _ __ | | __ _| |_ ___  ___  
-#   | |/ _ \ '_ ` _ \| '_ \| |/ _` | __/ _ \/ __| 
-#   | |  __/ | | | | | |_) | | (_| | ||  __/\__ \ 
-#   |_|\___|_| |_| |_| .__/|_|\__,_|\__\___||___/ 
-#                    |_|                          
-#  
-# by Stephan Raabe (2023) 
-# ----------------------------------------------------- 
+#  _____                    _       _
+# |_   _|__ _ __ ___  _ __ | | __ _| |_ ___  ___
+#   | |/ _ \ '_ ` _ \| '_ \| |/ _` | __/ _ \/ __|
+#   | |  __/ | | | | | |_) | | (_| | ||  __/\__ \
+#   |_|\___|_| |_| |_| .__/|_|\__,_|\__\___||___/
+#                    |_|
+# -----------------------------------------------------
 
 # Select text file
 selected=$(ls -1 ~/private/templates | rofi -dmenu -p "Select the template")
 
-
 if [ "$selected" ]; then
-    # Add content to clipboard
-    xclip -sel clip ~/private/templates/$selected
+	# Add content to clipboard
+	xclip -sel clip ~/private/templates/$selected
 fi
