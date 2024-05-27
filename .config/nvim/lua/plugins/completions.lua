7fa888cc11814c3acc1ba1ef0717f4462fba5597
Eklavya Sood <eklavyasood@gmail.com>
2024-05-27 13:36:56 +0530

    completions

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -28,12 +28,12 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-n>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort,
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          -- { name = "nvim_lsp" },
+          { name = "nvim_lsp" },
           { name = "luasnip" },
         }, {
             { name = "buffer" }
